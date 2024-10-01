894f40a50803515fab624c4bfdf789cf80594da2
heajun jeong <heajun35@gmail.com>
2024-10-01 18:50:03 +0900

    [Feat] - 사용했던 프로젝트의 버튼 스토리북 작성

---
+++ src/components/Button.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  onClick?: () => void;
+  onTypeClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  size?: "sm" | "md" | "big" | "extra";
+  border?: boolean;
+  color?: "black" | "gray3" | "primary" | "secondary" | "gray2";
+  disabled?: boolean;
+}
+
+const Button = ({
+  text,
+  onClick,
+  onTypeClick,
+  size,
+  border,
+  color,
+  disabled,
+}: ButtonProps) => {
+  const height =
+    (size === "sm" && "h-[22px]") ||
+    (size === "md" && "h-9") ||
+    (size === "big" && "h-12") ||
+    (size === "extra" && "h-14");
+
+  const col =
+    (color === "black" &&
+      border &&
+      "border border-solid border-black text-black") ||
+    (color === "gray2" &&
+      border &&
+      "border border-solid border-gray-2 text-black") ||
+    (color === "gray3" &&
+      border &&
+      "border border-solid border-gray-3 text-black") ||
+    (color === "primary" &&
+      border &&
+      "border border-solid border-primary-1 text-primary-1") ||
+    (color === "secondary" &&
+      border &&
+      "border border-solid border-secondary-1 text-secondary-1") ||
+    (color === "black" && "bg-black text-white") ||
+    (color === "gray2" && "bg-gray-2 text-white") ||
+    (color === "gray3" && "bg-gray-3 text-white") ||
+    (color === "primary" && "bg-primary-1 text-white") ||
+    (color === "secondary" && "bg-secondary-1 text-white");
+
+  const buttonClass = `flex items-center justify-center px-3 py-1 rounded-lg w-full ${height} ${col}`;
+
+  return (
+    <button
+      onClick={onClick ? onClick : onTypeClick}
+      className={[buttonClass, height, col].join(" ")}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,24 +1,26 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
 
-import { Button } from './Button';
+import Button from "@/components/Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    text: { control: "text" },
+    border: { control: "boolean" },
+    disabled: { control: "boolean" },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  args: { color: "primary", onClick: fn(), size: "big" },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -27,27 +29,79 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    color: "primary",
+    text: "Primary button",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    color: "secondary",
+    text: "Secondary button",
   },
 };
 
-export const Large: Story = {
+export const gray2: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    color: "gray2",
+    text: "Gray-2 button",
   },
 };
 
-export const Small: Story = {
+export const gray3: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    color: "gray3",
+    text: "Gray-3 button",
+  },
+};
+
+export const black: Story = {
+  args: {
+    color: "black",
+    text: "Black button",
+  },
+};
+
+export const sm: Story = {
+  args: {
+    size: "sm",
+    text: "sm button",
+  },
+};
+
+export const md: Story = {
+  args: {
+    size: "md",
+    text: "md button",
+  },
+};
+
+export const bg: Story = {
+  args: {
+    size: "bg",
+    text: "bg button",
+  },
+};
+
+export const extra: Story = {
+  args: {
+    size: "extra",
+    text: "Extra button",
+  },
+};
+
+export const border: Story = {
+  args: {
+    color: "primary",
+    border: true,
+    text: "Border button",
+  },
+};
+
+export const NoneBorder: Story = {
+  args: {
+    color: "primary",
+    border: false,
+    text: "Border button",
   },
 };
